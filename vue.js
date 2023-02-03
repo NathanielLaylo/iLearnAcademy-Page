48d71a19a607998226a47dad9b1c4892e80d32e6
NathanielLaylo <laylonathan1@yahoo.com>
2023-02-03 01:21:44 +0000

    Update vue.js

--- vue.js
+++ vue.js
@@ -184,7 +184,7 @@ var webstore = new Vue({
                 headers: {
                     "Content-Type": "application/json", //set the data type as JSON
                 },              
-                body: JSON.stringify(newProduct) //need to stringigy the JSON
+                body: JSON.stringify(order) //need to stringigy the JSON
             }).then(
                 function (response) {
                     response.json().then(
